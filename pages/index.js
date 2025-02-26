94e91ea5d6b4bcbf228774c7f5c0778359c72179
LexiLuthor5 <162353387+LexiLuthor5@users.noreply.github.com>
2025-02-26 12:56:48 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to Disney Trivia!</h1>
+        <p>Get ready to test your knowledge!</p>
+      </div>
+    );
+  }
+  
