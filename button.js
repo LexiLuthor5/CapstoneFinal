69b190756a6eb05a83ca96302a974455a3d46c76
LexiLuthor5 <162353387+LexiLuthor5@users.noreply.github.com>
2025-02-26 13:19:26 -0500

    Fix: Added missing components

--- button.js
+++ button.js
@@ -1,12 +1,11 @@
 export const Button = ({ children, className = "", onClick, ...props }) => {
-    return (
-      <button
-        onClick={onClick}
-        className={`py-2 px-4 rounded ${className}`}
-        {...props}
-      >
-        {children}
-      </button>
-    );
-  };
-  
+  return (
+    <button
+      onClick={onClick}
+      className={`py-2 px-4 rounded ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
